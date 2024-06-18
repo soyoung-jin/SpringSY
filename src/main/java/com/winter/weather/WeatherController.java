142974045537ccf0957d12e8c4177993b5c6c6a9
yangdanbi <dbyang302@gmail.com>
2024-06-18 15:30:38 +0900

    weather

---
+++ src/main/java/com/winter/weather/WeatherController.java
@@ -0,0 +1,14 @@
+package com.winter.weather;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WeatherController {
+
+	@RequestMapping("/weather/list")
+	public String getList() {
+		System.out.println("WeatherController : weatherList로 이동");
+		return "weather/list";
+	}
+}
